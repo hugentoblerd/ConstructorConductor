1646253fce936d3e9fdfdb16c839998bb9699a13
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-22 13:57:54 -0600

    completes arrayProperty.js

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ ConstructorConductor
 ##### Fork this repo, clone your fork, then do all of the tasks in the following order.
 1. instanceArray.js --> DONE
 2. sayName.js --> DONE
-3. arrayProperty.js
-4. quizApp.js
+3. arrayProperty.js -->  DONE
+4. quizApp.js --> 
--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,11 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
-  //code here
+ 
+var test = 'this is a test';
+new String(test);
+
+String.prototype.revStr =  function (str) {
+  str = str.split('').reverse().join('');
+  return str;
+}
